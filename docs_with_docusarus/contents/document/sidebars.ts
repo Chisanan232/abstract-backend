7d77414cb4215c4439cdf862c384803e3400cb84
Bryant <42397554+Chisanan232@users.noreply.github.com>
2025-10-24 12:09:35 +0800

    Initial commit

---
+++ docs_with_docusarus/contents/document/sidebars.ts
@@ -0,0 +1,41 @@
+import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
+
+/**
+ * Sidebar for the Docs section
+ */
+const sidebars: SidebarsConfig = {
+  docs: [
+    'introduction',
+    {
+      type: 'category',
+      label: '🤟 Quickly Start',
+      collapsed: false,
+      items: [
+        'quick-start/quick-start',
+        'quick-start/requirements',
+        'quick-start/installation',
+        'quick-start/how-to-run',
+      ],
+    },
+    {
+      type: 'category',
+      label: '🧑‍💻 API References',
+      items: [
+        'api-references/api-references',
+      ],
+    },
+    {
+      type: 'category',
+      label: '👋 Welcome to contribute',
+      items: [
+        'contribute/contribute',
+        'contribute/report-bug',
+        'contribute/request-changes',
+        'contribute/discuss',
+      ],
+    },
+    'changelog',
+  ],
+};
+
+export default sidebars;
